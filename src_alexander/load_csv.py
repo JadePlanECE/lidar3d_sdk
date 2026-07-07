0ff39d769d4ee622b6077971ecd8f5ec90b41a36
JadePlanECE <jade.planterose@edu.ece.fr>
2026-07-07 08:42:44 +0200

    fix loading csv

--- src_alexander/load_csv.py
+++ src_alexander/load_csv.py
@@ -2,10 +2,13 @@ import sys
 from pathlib import Path
 import pandas as pd
 
-DATA_DIR = Path("../data/alexander")
+DATA_DIR = Path("./data/alexander")
 
-def load_data_points(file_name="split_part_3"):
-    path = DATA_DIR / f"split_files/{file_name}.csv"
+def load_data_points(file_name, part_file):
+    if part_file is None:
+        path = DATA_DIR / f"points{file_name}.csv"
+    else:
+        path = DATA_DIR / f"points{file_name}/split_part_{part_file}.csv"
 
     if not path.exists():
         sys.exit(f"[Error] File not found {path}\n")
@@ -15,7 +18,7 @@ def load_data_points(file_name="split_part_3"):
     print(f"[Load] Loaded {len(df):,} rows from {path}")
     return df
 
-def load_data_imu(file_name="_20230315_152320-lidar1"):
+def load_data_imu(file_name):
     path = DATA_DIR / f"imu{file_name}.csv"
 
     if not path.exists():
